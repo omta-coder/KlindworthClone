f1632b8b2e6f66ffbca8a97aad2e84e7a34249ae
omta <omtadayma2001@gmail.com>
2024-04-12 10:16:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+gsap.from("#page1 img",{
+    width:"25%",
+    duration:2,
+    top:"88%",
+    scrollTrigger:{
+        trigger:"#page1",
+        scroller:"body",
+        start:"top 0%",
+        end:"top -80%",
+        scrub:2,
+        pin:true
+    }
+})
+
+
+
+var tl = gsap.timeline()
+
+tl.to(".overlay",{
+    top:0
+})
+tl.from(".overlay h3",{
+    y:60,
+    stagger: .3,
+    opacity:0
+})
+
+tl.pause()
+
+let menu = document.querySelector("#menu")
+let nav = document.querySelector("#nav")
+
+let menuVisible = false;
+
+menu.addEventListener("click", () => {
+    if (menuVisible) {
+        tl.reverse();
+        menuVisible = false;
+    } else {
+        tl.play();
+        menuVisible = true;
+    }
+});
+
+
+var element = document.querySelectorAll('.element')
+
+element.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        gsap.to("#more",{
+            opacity:1,
+            transform: `translate(-50%,-50%) scale(1)`
+
+        })
+    })
+    e.addEventListener("mouseleave",function(){
+        gsap.to("#more",{
+            opacity:0,
+            transform: `translate(-50%,-50%) scale(0)`
+
+        })
+    })
+    })
+   
+    var page2Elements = document.querySelector(".page2-elements")
+    
+    var rect = page2Elements.getBoundingClientRect()
+    
+    let elemX = rect.x
+    let elemY = rect.y
+    console.log(elemX + " " + elemY);
+
+    page2Elements.addEventListener("mousemove",function(dets){
+        gsap.to("#more",{
+            x:dets.x-elemX, 
+            y:dets.y-elemY
+        })
+        console.log(dets.x);
+        console.log(dets.y);
+    })
