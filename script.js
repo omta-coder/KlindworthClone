2dcbd938a65abcbce59dbf9aa6dd0ebbb586835b
omta <omtadayma2001@gmail.com>
2024-04-22 21:27:29 +0530

    add page4

--- script.js
+++ script.js
@@ -115,3 +115,9 @@ element.forEach(function(e){
             scrub:true
         }
     })
+    var swiper = new Swiper(".mySwiper", {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
